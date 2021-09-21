dcc0f628f0c8f5407ba10ef7f905f77bf4f32d3c
lxhillwind <lxhillwind@gmail.com>
2021-09-21 19:26:49 +0800

    remove msys2 vim

--- package.sh
+++ package.sh
@@ -18,11 +18,6 @@ for ARCH in i686 x86_64; do
     cp -r etc "$ARCH"/
     pacman --config pacman.conf --arch "$ARCH" -r "$ARCH" -Sy
     pacman --config pacman.conf --arch "$ARCH" -r "$ARCH" -S --noconfirm $(grep -v '^#' pkglist.txt)
-
-    cp -a "$ARCH" "$ARCH"-new
-    pacman --config pacman.conf --arch "$ARCH" -r "$ARCH" -S --noconfirm vim
-    cp "$ARCH"/usr/bin/vim.exe "$ARCH"-new/usr/bin/vim.exe
-
-    (cd "$ARCH"-new && 7z a ../"$ARCH".7z etc usr;)
+    (cd "$ARCH" && 7z a ../"$ARCH".7z etc usr;)
 done
 EOF
