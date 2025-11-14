7bec2237f65de2ae7566009a1dfc1f9209db3291
lxhillwind <lxhillwind@gmail.com>
2025-11-15 01:52:35 +0800

    Fix MSYS2 keyring download URL in package.sh
    Updated the URL for downloading the MSYS2 keyring from 'master' to 'main'.

--- package.sh
+++ package.sh
@@ -8,12 +8,12 @@ docker run --rm -i -v "$PWD":/data/ -w /data alpine:latest sh -s <<\EOF
 apk add pacman p7zip
 
 mkdir -p /usr/share/pacman/keyrings
-wget https://github.com/msys2/MSYS2-keyring/raw/master/msys2.gpg -O /usr/share/pacman/keyrings/msys2.gpg
+wget https://github.com/msys2/MSYS2-keyring/raw/main/msys2.gpg -O /usr/share/pacman/keyrings/msys2.gpg
 
 pacman-key --config pacman.conf --init
 pacman-key --config pacman.conf --populate msys2
 
-for ARCH in i686 x86_64; do
+for ARCH in x86_64; do
     mkdir -p "$ARCH"/var/lib/pacman
     cp -r etc "$ARCH"/
     pacman --config pacman.conf --arch "$ARCH" -r "$ARCH" -Sy
